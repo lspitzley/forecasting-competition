6fb6d34aa5e64c0cef403af672afd9a1baa35fa7
Lee Spitzley <lee.spitzley@gmail.com>
2020-10-06 09:25:19 -0400

    slack uploader executable

--- slack_upload.py
+++ slack_upload.py
@@ -11,12 +11,6 @@ import requests
 import yaml
 #%% upload file
 
-filepath = "tokens.yaml"
-with open(filepath, 'r') as f:
-    keys = yaml.safe_load(f)
-# post data to Slack
-token = keys['token']
-print(token)
 
 #%% upload forecast file to Slack
 def upload_forecast_file(token):
@@ -36,51 +30,110 @@ def upload_forecast_file(token):
 
 
 #%% read file to bot
-payload ={
-  "token":token, 
-  "channels":'#B01AJRTQJLE',
-  "types" : "csv"}
-headers = {'Content-Type': 'application/json'}
-get = requests.get('https://slack.com/api/files.list', params=payload, headers=headers)
-print(get)
-print(get.json()["files"][0]["name"])
-print(get.text)
-# with open()
+def read_bot_submission(token):
+    """
+    Download files sent to the bot
+    
+    Not functional
+
+    Args:
+        token (TYPE): DESCRIPTION.
+
+    Returns:
+        None.
+
+    """
+    payload ={
+      "token":token, 
+      "channels":'#B01AJRTQJLE',
+      "types" : "csv"}
+    headers = {'Content-Type': 'application/json'}
+    get = requests.get('https://slack.com/api/files.list', params=payload, headers=headers)
+    print(get)
+    print(get.json()["files"][0]["name"])
+    print(get.text)
+    # with open()
 
 
 #%% files to the bot
-# U01ARHWDLN6 # bot
-# user: U018ZK7MD7D
-payload ={
-  "token":token, 
- "channels":'U01ARHWDLN6'}
-headers = {'Content-Type': 'application/json'}
-get = requests.get('https://slack.com/api/conversations.members', params=payload)
-print(get)
-#print(get.json()["files"][0]["name"])
-print(get.text)
-for convo in get.json()["channels"]:
-    print(convo["name"])
+def view_bot_conversations(token):
+    """
+    View people who have interacted with the bot
+
+    Args:
+        token (TYPE): DESCRIPTION.
+
+    Returns:
+        None.
+
+    """
+    # U01ARHWDLN6 # bot
+    # user: U018ZK7MD7D
+    payload ={
+      "token":token, 
+     "channels":'U01ARHWDLN6'}
+    headers = {'Content-Type': 'application/json'}
+    get = requests.get('https://slack.com/api/conversations.members', params=payload)
+    print(get)
+    #print(get.json()["files"][0]["name"])
+    print(get.text)
+    for convo in get.json()["channels"]:
+        print(convo["name"])
 
 
 #%% convo list
-payload ={
-  "token":token,
-  "types": "im",
-  "channel": "D01AJSEBX38"}
-headers = {'Content-Type': 'application/json'}
-get = requests.get('https://slack.com/api/conversations.history', params=payload)
-print(get)
-#print(get.json()["files"][0]["name"])
-print(get.text)
-for convo in get.json()["channel"]:
-    print(convo["user"])
+def view_convo_history(token):
+    """
+    See what has been sent to the bot
+    
+    Not working?
+
+    Args:
+        token (TYPE): DESCRIPTION.
+
+    Returns:
+        None.
+
+    """
+    payload ={
+      "token":token,
+      "types": "im",
+      "channel": "D01AJSEBX38"}
+    headers = {'Content-Type': 'application/json'}
+    get = requests.get('https://slack.com/api/conversations.history', params=payload)
+    print(get)
+    #print(get.json()["files"][0]["name"])
+    print(get.text)
+    for convo in get.json()["channel"]:
+        print(convo["user"])
 
 
 
 #%% get bot info
-payload = {"token":token, 
-}
-bot= requests.get('https://slack.com/api/auth.test', params=payload)
-print(bot.json())
-print(bot.text)
+def get_bot_info(token):
+    """
+    Get the bot ID
+
+    Args:
+        token (TYPE): DESCRIPTION.
+
+    Returns:
+        None.
+
+    """
+    payload = {"token":token, 
+    }
+    bot= requests.get('https://slack.com/api/auth.test', params=payload)
+    print(bot.json())
+    print(bot.text)
+
+#%% run
+if __name__ == '__main__':
+    # load up the token
+    filepath = "tokens.yaml"
+    with open(filepath, 'r') as f:
+        keys = yaml.safe_load(f)
+    # post data to Slack
+    token = keys['token']
+    
+    upload_forecast_file(token)
