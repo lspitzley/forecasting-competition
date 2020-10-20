20df1c53682d4927bdcf670d00826af7793e7796
Lee Spitzley <lspitzley@albany.edu>
2020-10-20 13:45:48 -0400

    updated eval script

--- R code/forecast_evaluation.R
+++ R code/forecast_evaluation.R
@@ -1,9 +1,12 @@
+#### INSTRUCTIONS ---------------------------------------
+# Run this file from the top-level directory.
+# Forecasts should be in 'forecasts' directory
 #### evaluation ------------------------------------------
 library(reshape2)
 library(forecast)
 library(plyr)
 library(lubridate)
-source('forecast_functions.R')
+source('R code/forecast_functions.R')
 
 # load files into a single data frame
 last_ten_years <- readRDS('last_ten_alb.rds')
@@ -11,48 +14,47 @@ last_ten_years <- readRDS('last_ten_alb.rds')
 file_list <- list.files('forecasts/', pattern = '.csv|.CSV')
 raw_files <- lapply(file_list, function(x) {print(x)
   read.csv(paste0('forecasts/', x), stringsAsFactors = FALSE)})
-pred.df <- do.call(rbind.fill, raw_files)
+pred_df <- do.call(rbind.fill, raw_files)
 
 #### DATA CLEANING -----------------------------------------------------------------------------
 
 # fix dates (only works if dates are in fc.date or fc.dates)
-pred.df$fc.date <- ifelse(is.na(pred.df$fc.date), pred.df$fc.dates, pred.df$fc.date)
-pred.df$fixed.date <- parse_date_time(x = pred.df$fc.date, c('ymd', 'mdy'))
+pred_df$fc_date <- ifelse(is.na(pred_df$fc_date), pred_df$fc.date, pred_df$fc_date)
+pred_df$fixed_date <- parse_date_time(x = pred_df$fc_date, c('ymd', 'mdy'))
 
 # fix mislabled P_PRCP var
-pred.df[which(pred.df$fc.var == 'P_PRCP' | pred.df$fc.var == 'PRCP'), 'fc.var'] <- 'I_PRCP'
+pred_df[which(pred_df$fc_var == 'P_PRCP' | pred_df$fc_var == 'PRCP'), 'fc_var'] <- 'I_PRCP'
 
 # clean up bad values
-pred.df$fc.value <- as.numeric(pred.df$fc.value)
+pred_df$fc_value <- as.numeric(pred_df$fc_value)
 
 # adjust names to be lowercase
-pred.df$fc.name <- tolower(pred.df$fc.name)
+pred_df$fc_name <- tolower(pred_df$fc_name)
 
 # clean up columns and remove duplicates
-fc.cols <- c('fc.name', 'fc.var', 'fixed.date', 'fc.value')
-pred.df <- pred.df[complete.cases(pred.df[,fc.cols]),fc.cols]
-pred.df <- pred.df[!duplicated(pred.df[,c('fc.name', 'fc.var', 'fixed.date')]),]
+fc_cols <- c('fc_name', 'fc_var', 'fixed_date', 'fc_value')
+pred_df <- pred_df[complete.cases(pred_df[,fc_cols]),fc_cols]
+pred_df <- pred_df[!duplicated(pred_df[,c('fc_name', 'fc_var', 'fixed_date')]),]
 
 ##### merge historical data with predictions  --------------------------------------------------
 eval_df <- melt(data = last_ten_years, id.vars = c('YEARMODA'), 
                 measure.vars = c('MAX', 'MIN', 'I_PRCP'), 
                 value.name = 'obs.value')
-pred_eval <- merge(x = pred.df, y=eval_df, 
-                   by.x = c('fixed.date', 'fc.var'),
+pred_eval <- merge(x = pred_df, y=eval_df, 
+                   by.x = c('fixed_date', 'fc_var'),
                    by.y = c('YEARMODA', 'variable'))
 
 #### evaluate by entry/week --------------------------------------------------------------------
 
-week_beginning <- c('2019-02-11', '2019-02-18', '2019-02-25', '2019-03-04', '2019-03-11', 
-                    '2019-03-18', '2019-03-25', '2019-04-02')
+week_beginning <- c('2020-09-30')
 for (week in week_beginning) {
   week_of <- date(week)
   print(week_of)
-  week_prediction <- subset(pred_eval, fixed.date >= week_of &fixed.date <= week_of + days(6))
+  week_prediction <- subset(pred_eval, fixed_date >= week_of &fixed_date <= week_of + days(7))
   if (nrow(week_prediction) == 0) break
-  unique_entries <- as.data.frame(unique(week_prediction[,c('fc.name', 'fc.var')]))
-  temperature_entries <- subset(unique_entries, !is.na(fc.name) & fc.var %in% c('MAX', 'MIN'))
-  precip_entries <- subset(unique_entries, !is.na(fc.name) & fc.var == 'I_PRCP')
+  unique_entries <- as.data.frame(unique(week_prediction[,c('fc_name', 'fc_var')]))
+  temperature_entries <- subset(unique_entries, !is.na(fc_name) & fc_var %in% c('MAX', 'MIN'))
+  precip_entries <- subset(unique_entries, !is.na(fc_name) & fc_var == 'I_PRCP')
   eval_results <- lapply(1:nrow(temperature_entries), 
                          FUN = function(x) group_forecast_eval(week_prediction,
                                                                temperature_entries[x,1],
@@ -78,11 +80,11 @@ for (week in week_beginning) {
 }
 
 #### overall predictions ----------------------------------------------------------------------
-full_prediction <- subset(pred_eval, fixed.date >= date('2019-02-11'))
+full_prediction <- subset(pred_eval, fixed_date >= date('2020-09-30'))
 
-unique_entries <- as.data.frame(unique(full_prediction[,c('fc.name', 'fc.var')]))
-temperature_entries <- subset(unique_entries, !is.na(fc.name) & fc.var %in% c('MAX', 'MIN'))
-precip_entries <- subset(unique_entries, !is.na(fc.name) & fc.var == 'I_PRCP')
+unique_entries <- as.data.frame(unique(full_prediction[,c('fc_name', 'fc_var')]))
+temperature_entries <- subset(unique_entries, !is.na(fc_name) & fc_var %in% c('MAX', 'MIN'))
+precip_entries <- subset(unique_entries, !is.na(fc_name) & fc_var == 'I_PRCP')
 eval_results <- lapply(1:nrow(temperature_entries), 
                        FUN = function(x) group_forecast_eval(full_prediction,
                                                              temperature_entries[x,1],
@@ -105,3 +107,4 @@ prcp_results_df$as_of <- Sys.Date()
 rownames(prcp_results_df) <- NULL
 print(prcp_results_df)
 write.csv(prcp_results_df, paste0('output/precip-results_cumulative.csv'))
+
